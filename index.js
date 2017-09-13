15f218360ab26ee2a6c2a8a4400be1fdb8f33715
marcodagolini <marco.dagolini@gmail.com>
2017-09-13 17:36:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Content-Type', 'text/plain');
+  res.header("Content-Type", "text/plain");
   next();
 });
 

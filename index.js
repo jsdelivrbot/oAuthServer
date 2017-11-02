be22bc8b6cf35fa423a3ff9874522748fbb55fc9
marcodagolini <marco.dagolini@gmail.com>
2017-11-02 18:02:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var https = require('https');
 
 
-var cert_priv = fs.readFileSync('pkcs8.key');
+var cert_priv = fs.readFileSync('private.key');
 var cert_pub = fs.readFileSync('publickey.crt');
 
 
